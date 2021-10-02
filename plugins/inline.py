36eb085253d7d22f8b078f133b80166c29825fd5
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-10-02 14:41:13 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Group', url='https://t.me/movieshub_group')
+            InlineKeyboardButton('Channel', url='https://t.me/movies_hub66')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
