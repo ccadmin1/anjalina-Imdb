fb1cf2f58df683bedd2d34a7e209ec8fab75426c
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-10-10 19:43:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -362,4 +362,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("Nice.. Next Time Sure You Will Get It 🤭😝",show_alert=True)
+        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
